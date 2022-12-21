c11ee2e6d98d6768b2e0f5c94b417ee81dbc7b4b
Shubham Nandanwar <shubham.nandanwar1995@gmail.com>
2022-12-21 07:52:57 +0530

    Handling form data with @RequestParameter and ${param}

---
+++ SpringMVC/WebContent/WEB-INF/view/userform.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>User form</title>
+</head>
+<body>
+<form action="showdata" method="Post">
+	<input type="text" name="name"><br/><br/>
+	<input type="submit" />
+</form>
+</body>
+</html>
--- SpringMVC/src/main/java/com/example/controller/HelloController.java
+++ SpringMVC/src/main/java/com/example/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.controller;
 
+import java.net.http.HttpResponse;
+
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/hello")
-	public String hello() {
-		System.out.println("hELLO");
-		return "welcome";
+	@RequestMapping(value = "/userform",method = RequestMethod.GET)
+	public String userform() {
+		
+		return "userform";
 	}
+	@RequestMapping(value = "/showdata",method = RequestMethod.POST)
+	public String showdata(@RequestParam(name = "name") String name) {
+			return "userdata";
+		}
 }
